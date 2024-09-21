8e23ec78697510828949033ff7b71a5840090361
Zach <zijiebing@gmail.com>
2024-09-21 18:47:31 +0800

    bugfix

--- lua/telescope/_extensions/tabpicker.lua
+++ lua/telescope/_extensions/tabpicker.lua
@@ -10,12 +10,16 @@ local conf = {
 }
 
 local tabpicker = require("tabpicker.builtin")
+local actions = require("tabpicker.builtin.actions")
 return require("telescope").register_extension({
     setup = function(ext_config, config) conf = vim.tbl_deep_extend("force", conf, ext_config) end,
     exports = {
         find_tabpages = function(opts)
             opts = vim.tbl_extend("force", conf, opts or {})
-            return tabpicker.find_tabpages(opts)
+            return tabpicker.pickers.find_tabpages(opts)
         end,
+        actions = {
+            close_tab = actions.close_tab,
+        },
     },
 })
